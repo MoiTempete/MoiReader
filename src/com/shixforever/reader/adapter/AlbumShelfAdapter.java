dfb9a225bb5bb984bd4b30bdf9fd2265d2c3ef7f
侯智 <moitempete@gmail.com>
2014-08-06 19:47:04 +0800

    modify

--- src/com/shixforever/reader/adapter/AlbumShelfAdapter.java
+++ src/com/shixforever/reader/adapter/AlbumShelfAdapter.java
@@ -13,54 +13,56 @@ import com.shixforever.reader.data.BookFile;
 import com.shixforever.reader.utils.FileTools;
 
 public class AlbumShelfAdapter extends BaseAdapter {
-	private List<BookFile> albums;
-	private Context context;
+    
+    private List<BookFile> albums;
 
-	public AlbumShelfAdapter(List<BookFile> albums, Context context) {
-		super();
-		this.albums = albums;
-		this.context = context;
-	}
+    private Context context;
 
-	@Override
-	public int getCount() {
-		return albums.size();
-	}
+    public AlbumShelfAdapter(List<BookFile> albums, Context context) {
+        super();
+        this.albums = albums;
+        this.context = context;
+    }
 
-	@Override
-	public Object getItem(int position) {
-		return albums.get(position);
-	}
+    @Override
+    public int getCount() {
+        return albums.size();
+    }
 
-	@Override
-	public long getItemId(int position) {
-		return position;
-	}
+    @Override
+    public Object getItem(int position) {
+        return albums.get(position);
+    }
 
-	@Override
-	public View getView(int position, View convertView, ViewGroup parent) {
-		TextView tvAlbumName;
-		if (convertView == null) {
-			convertView = LayoutInflater.from(context).inflate(
-					R.layout.album_item, null);
-		}
-		tvAlbumName = (TextView) convertView.findViewById(R.id.tvAlbumName);
-		BookFile album = albums.get(position);
-		if (album.flag.equals("1")) {
-			tvAlbumName.setBackgroundResource(R.drawable.cover_txt);
-			tvAlbumName.setText(album.name);
-		} else if (album.cover.equals("0")) {
-			tvAlbumName.setBackgroundResource(R.drawable.cover_txt);
-			tvAlbumName.setText(album.name);
-		} else {
-			tvAlbumName.setBackgroundResource(FileTools.getResource(context,
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        TextView tvAlbumName;
+        if (convertView == null) {
+            convertView = LayoutInflater.from(context).inflate(
+                    R.layout.album_item, null);
+        }
+        tvAlbumName = (TextView) convertView.findViewById(R.id.tvAlbumName);
+        BookFile album = albums.get(position);
+        if (album.flag.equals("1")) {
+            tvAlbumName.setBackgroundResource(R.drawable.cover_txt);
+            tvAlbumName.setText(album.name);
+        } else if (album.cover.equals("0")) {
+            tvAlbumName.setBackgroundResource(R.drawable.cover_txt);
+            tvAlbumName.setText(album.name);
+        } else {
+            tvAlbumName.setBackgroundResource(FileTools.getResource(context,
                     album.cover));
-		}
-		return convertView;
-	}
+        }
+        return convertView;
+    }
 
-	public void change(List<BookFile> album) {
-		this.albums = album;
-		notifyDataSetChanged();
-	}
+    public void change(List<BookFile> album) {
+        this.albums = album;
+        notifyDataSetChanged();
+    }
 }
