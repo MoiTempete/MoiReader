dd6c53cd8266a2f7575267418d2705f1e7b5b84a
侯智 <moitempete@gmail.com>
2014-08-06 19:45:06 +0800

    modify

--- src/com/shixforever/reader/activity/BookShelfActivity.java
+++ src/com/shixforever/reader/activity/BookShelfActivity.java
@@ -12,7 +12,6 @@ import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.TextView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.AdapterView.OnItemLongClickListener;
 import android.widget.GridView;
@@ -27,62 +26,69 @@ import com.shixforever.reader.utils.Tools;
 import com.shixforever.reader.db.DBManager;
 
 public class BookShelfActivity extends BaseActivity {
-	private GridView bookShelf;
-	private AlbumShelfAdapter albumShelfAdapter;
-	private DBManager mgr;
-	private List<BookFile> books;
-	/**
-	 * book File cache
-	 */
-	private File filecatch;
-	private String filecatchpath = "/data/data/"
-			+ FusionField.baseActivity.getPackageName() + "/";
-	private boolean isConnected;
-	private boolean nhFlag = false;
+    private GridView bookShelf;
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
-		isConnected = isWifiConnect();
+    private AlbumShelfAdapter albumShelfAdapter;
 
-		// 预加载
-		initView();
-		mgr = new DBManager(this);
-		initData();
-	}
+    private DBManager mgr;
 
-	@Override
-	protected void onResume() {
-		super.onResume();
-	}
+    private List<BookFile> books;
 
-	@Override
-	protected void onRestart() {
-		this.notifyDataChange();
-		super.onRestart();
-	}
+    /**
+     * book File cache
+     */
+    private File filecatch;
 
-	private void initView() {
-		bookShelf = (GridView) findViewById(R.id.bookShelf);
-	}
+    private String filecatchpath = "/data/data/"
+            + FusionField.baseActivity.getPackageName() + "/";
 
-	private void notifyDataChange() {
-	}
+    private boolean isConnected;
 
-	private void initData() {
-		// initialize DB
-		books = mgr.queryPro();
-		if (!nhFlag) {
-			for (int i = 0; i < books.size(); i++) {
-				if (books.get(i).flag.equals("3"))
-					books.remove(i);
-			}
-		}
-		File file = new File(FileManager.FILE_SDCARD_PATH);
-		if (file.isDirectory()) {
-			File[] fileArray = file.listFiles();
-			if (null != fileArray && 0 != fileArray.length) {
+    private boolean nhFlag = false;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        isConnected = isWifiConnect();
+
+        // 预加载
+        initView();
+        mgr = new DBManager(this);
+        initData();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onRestart() {
+        this.notifyDataChange();
+        super.onRestart();
+    }
+
+    private void initView() {
+        bookShelf = (GridView) findViewById(R.id.bookShelf);
+    }
+
+    private void notifyDataChange() {
+    }
+
+    private void initData() {
+        // initialize DB
+        books = mgr.queryPro();
+        if (!nhFlag) {
+            for (int i = 0; i < books.size(); i++) {
+                if (books.get(i).flag.equals("3"))
+                    books.remove(i);
+            }
+        }
+        File file = new File(FileManager.FILE_SDCARD_PATH);
+        if (file.isDirectory()) {
+            File[] fileArray = file.listFiles();
+            if (null != fileArray && 0 != fileArray.length) {
                 for (File aFileArray : fileArray) {
                     BookFile bookFile = new BookFile();
                     bookFile.path = aFileArray.getPath();
@@ -96,122 +102,122 @@ public class BookShelfActivity extends BaseActivity {
                     books.add(bookFile);
                 }
 
-			}
-		}
-		albumShelfAdapter = new AlbumShelfAdapter(books, this);
-		bookShelf.setAdapter(albumShelfAdapter);
-		bookShelf.setOnItemClickListener(new OnItemClickListener() {
-			@Override
-			public void onItemClick(AdapterView<?> parent, View view,
-					int position, long id) {
-				BookFile albumitem = (BookFile) parent
-						.getItemAtPosition(position);
-				// 传递file会崩溃
-				BookFile album = new BookFile();
-				album.id = albumitem.id;
-				album.name = albumitem.name;
-				album.cover = albumitem.cover;
-				album.path = albumitem.path;
-				album.flag = albumitem.flag;
-				System.out.println(filecatchpath);
-				if (albumitem.flag.equals("0")) {
-					filecatch = Tools.getFile(albumitem.file, filecatchpath,
+            }
+        }
+        albumShelfAdapter = new AlbumShelfAdapter(books, this);
+        bookShelf.setAdapter(albumShelfAdapter);
+        bookShelf.setOnItemClickListener(new OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                    int position, long id) {
+                BookFile albumitem = (BookFile) parent
+                        .getItemAtPosition(position);
+                // 传递file会崩溃
+                BookFile album = new BookFile();
+                album.id = albumitem.id;
+                album.name = albumitem.name;
+                album.cover = albumitem.cover;
+                album.path = albumitem.path;
+                album.flag = albumitem.flag;
+                System.out.println(filecatchpath);
+                if (albumitem.flag.equals("0")) {
+                    filecatch = Tools.getFile(albumitem.file, filecatchpath,
                             "catch.txt");
-					if (filecatch == null) {
-						System.err.println("null");
-					}
-				}
-				Intent intent = new Intent();
-				Bundle bundle = new Bundle();
-				bundle.putSerializable("path", album);
-				intent.putExtras(bundle);
-				intent.setClass(BookShelfActivity.this, BookActivity.class);
-				startActivity(intent);
-			}
-		});
-		bookShelf.setOnItemLongClickListener(new OnItemLongClickListener() {
+                    if (filecatch == null) {
+                        System.err.println("null");
+                    }
+                }
+                Intent intent = new Intent();
+                Bundle bundle = new Bundle();
+                bundle.putSerializable("path", album);
+                intent.putExtras(bundle);
+                intent.setClass(BookShelfActivity.this, BookActivity.class);
+                startActivity(intent);
+            }
+        });
+        bookShelf.setOnItemLongClickListener(new OnItemLongClickListener() {
 
-			@Override
-			public boolean onItemLongClick(final AdapterView<?> parent,
-					View view, final int position, long id) {
-				AlertDialog.Builder builder = new Builder(
-						BookShelfActivity.this);
-				builder.setMessage("删除");
-				builder.setTitle("确认删除？");
-				builder.setPositiveButton("确认",
-						new DialogInterface.OnClickListener() {
-							@Override
-							public void onClick(DialogInterface dialog,
-									int which) {
-								BookFile bookFile = (BookFile) parent
-										.getItemAtPosition(position);
+            @Override
+            public boolean onItemLongClick(final AdapterView<?> parent,
+                    View view, final int position, long id) {
+                AlertDialog.Builder builder = new Builder(
+                        BookShelfActivity.this);
+                builder.setMessage("删除");
+                builder.setTitle("确认删除？");
+                builder.setPositiveButton("确认",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog,
+                                    int which) {
+                                BookFile bookFile = (BookFile) parent
+                                        .getItemAtPosition(position);
 
-								if (bookFile.flag.equals("0")) {
-									// 删除
-									mgr.deleteBook((BookFile) parent
-											.getItemAtPosition(position));
-								} else {
-									File file = new File(
-											FileManager.FILE_SDCARD_PATH
-													+ bookFile.name);
-									if (file.isFile()) {
-										file.delete();
-									}
-								}
-								// 刷新
-								albumShelfAdapter.change(refresh());
-								Toast.makeText(BookShelfActivity.this, "删除完成",
-										Toast.LENGTH_SHORT).show();
-							}
-						});
-				builder.setNegativeButton("取消",
-						new DialogInterface.OnClickListener() {
-							@Override
-							public void onClick(DialogInterface dialog,
-									int which) {
-							}
-						});
-				builder.create().show();
-				return true;
-			}
-		});
-	}
+                                if (bookFile.flag.equals("0")) {
+                                    // 删除
+                                    mgr.deleteBook((BookFile) parent
+                                            .getItemAtPosition(position));
+                                } else {
+                                    File file = new File(
+                                            FileManager.FILE_SDCARD_PATH
+                                                    + bookFile.name);
+                                    if (file.isFile()) {
+                                        file.delete();
+                                    }
+                                }
+                                // 刷新
+                                albumShelfAdapter.change(refresh());
+                                Toast.makeText(BookShelfActivity.this, "删除完成",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                builder.setNegativeButton("取消",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog,
+                                    int which) {
+                            }
+                        });
+                builder.create().show();
+                return true;
+            }
+        });
+    }
 
-	@Override
-	public boolean onKeyDown(int keyCode, KeyEvent event) {
-		return super.onKeyDown(keyCode, event);
-	}
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        return super.onKeyDown(keyCode, event);
+    }
 
-	@Override
-	protected void onDestroy() {
-		super.onDestroy();
-		mgr.closeDB();
-	}
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mgr.closeDB();
+    }
 
-	/**
-	 * 判断wifi是否连接
-	 * 
-	 * @return
-	 */
-	public boolean isWifiConnect() {
-		ConnectivityManager connManager = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
-		NetworkInfo mWifi = connManager
-				.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
-		return mWifi.isConnected();
-	}
+    /**
+     * 判断wifi是否连接
+     *
+     * @return
+     */
+    public boolean isWifiConnect() {
+        ConnectivityManager connManager = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+        NetworkInfo mWifi = connManager
+                .getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+        return mWifi.isConnected();
+    }
 
-	private List<BookFile> refresh() {
-		List<BookFile> booksdata = mgr.queryPro();
-		if (!nhFlag) {
-			for (int i = 0; i < booksdata.size(); i++) {
-				if (booksdata.get(i).flag.equals("3"))
-					booksdata.remove(i);
-			}
-		}
-		File file = new File(FileManager.FILE_SDCARD_PATH);
-		if (file.isDirectory()) {
-			File[] fileArray = file.listFiles();
-			if (null != fileArray && 0 != fileArray.length) {
+    private List<BookFile> refresh() {
+        List<BookFile> booksdata = mgr.queryPro();
+        if (!nhFlag) {
+            for (int i = 0; i < booksdata.size(); i++) {
+                if (booksdata.get(i).flag.equals("3"))
+                    booksdata.remove(i);
+            }
+        }
+        File file = new File(FileManager.FILE_SDCARD_PATH);
+        if (file.isDirectory()) {
+            File[] fileArray = file.listFiles();
+            if (null != fileArray && 0 != fileArray.length) {
                 for (File aFileArray : fileArray) {
                     BookFile bookFile = new BookFile();
                     bookFile.path = aFileArray.getPath();
@@ -225,8 +231,8 @@ public class BookShelfActivity extends BaseActivity {
                     booksdata.add(bookFile);
                 }
 
-			}
-		}
-		return booksdata;
-	}
+            }
+        }
+        return booksdata;
+    }
 }
